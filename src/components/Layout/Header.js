c8fd72b2e3f48d483df9d29f547e60e61390f3c5
Sulav23 <sulav.shrestha@xrdig.com>
2023-01-05 13:32:21 +0545

    Header with cart icon added

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import mealImg from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = () => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>Meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealImg} />
+      </div>
+    </>
+  );
+};
+
+export default Header;
